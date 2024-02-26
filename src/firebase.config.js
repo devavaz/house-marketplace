6406fdcb78096e693cc3bf092a7a84e34696f7f8
devavaz <135300758+devavaz@users.noreply.github.com>
2024-02-27 02:20:02 +0900

    Added Navbar and implemented styling using useNavigate, useLocation hooks

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4PqerAIfSOfYPYPFuMufRVoufd-P10e8",
+  authDomain: "house-marketplace-app-9fbc4.firebaseapp.com",
+  projectId: "house-marketplace-app-9fbc4",
+  storageBucket: "house-marketplace-app-9fbc4.appspot.com",
+  messagingSenderId: "466069814918",
+  appId: "1:466069814918:web:cf6e43230a106edd539b07"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
